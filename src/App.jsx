f9c10c2cc6f04d3e5b797228a68d370fca42f010
Rodgers Chayuga <rodgers@food4education.org>
2024-01-11 11:45:08 +0300

    Feat: Create clean folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-flex-row">
+      Hello world!
+    </div>
+  );
 };
 
 export default App;
